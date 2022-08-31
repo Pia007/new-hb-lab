b145d8989ac2164be3fe2108beb11cb9b99ab2b6
Pia <piatorain@yahoo.com>
2022-08-31 14:54:07 -0700

    Deploying

--- server/index.js
+++ server/index.js
@@ -3,8 +3,10 @@ const path = require('path');
 const app = express();
 
 
+app.use(express.static(path.join(__dirname, '../public')));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'));
+
 });
 
 const port = process.env.PORT || 4005;
